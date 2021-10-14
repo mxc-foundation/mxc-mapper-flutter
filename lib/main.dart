b3fba519c66d819b97ca34e5406d1b2b6224fee3
NamGyeong <namgyeong.cho@mxc.org>
2021-10-14 09:17:36 +0200

    MMD-1592 deleted commneted

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:mxc_mapper/constants.dart';
 import 'package:mxc_mapper/controllers/MenuController.dart';
-// import 'package:mxc_mapper/screens/main/main_screen.dart';
-import 'package:mxc_mapper/responsive.dart';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:provider/provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mxc_mapper/screens/main/components/side_menu.dart';
 import 'package:mxc_mapper/screens/main/main_screen.dart';
-import 'package:mxc_mapper/screens/map/map_screen.dart';
 
 void main() {
   // riverpod
@@ -19,10 +13,6 @@ void main() {
 }
 
 final menuProvider = ChangeNotifierProvider((ref) => MenuController());
-
-/* void main() {
-  runApp(MyApp());
-} */
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -37,31 +27,9 @@ class MyApp extends StatelessWidget {
             .apply(bodyColor: Colors.white),
         canvasColor: secondaryColor,
       ),
-      /* home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (context) => MenuController(),
-          ),
-        ],
-        child: MainScreen(),
-      ), */
-
       home: Consumer(builder: (context, watch, child) {
         // final menuController = watch(menuProvider);
         return MainScreen();
-        /* Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (Responsive.isDesktop(context))
-              Expanded(
-                flex: 5,
-                child: MapScreen(),
-              ),
-            Expanded(
-              child: SideMenu(),
-            ),
-          ],
-        ); */
       }),
     );
   }
