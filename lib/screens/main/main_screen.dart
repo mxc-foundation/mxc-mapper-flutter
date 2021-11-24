3c7bd87461d3e4ee03670503942ad5c64d08696b
NamGyeong <namgyeong.cho@mxc.org>
2021-11-24 09:56:54 +0100

    MMD-1747 removed unused widget

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,9 +1,7 @@
-import 'package:mxc_mapper/controllers/MenuController.dart';
+// import 'package:mxc_mapper/controllers/MenuController.dart';
 import 'package:mxc_mapper/responsive.dart';
 import 'package:mxc_mapper/screens/map/map_screen.dart';
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-
 import 'components/side_menu.dart';
 
 class MainScreen extends StatelessWidget {
@@ -24,31 +22,3 @@ class MainScreen extends StatelessWidget {
     );
   }
 }
-
-
-/* class MainScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        key: context.read<MenuController>().scaffoldKey,
-        drawer: Responsive.isDesktop(context) ? null : SideMenu(),
-        body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (Responsive.isDesktop(context))
-                Expanded(
-                  flex: 5,
-                  child: MapScreen(),
-                ),
-              Expanded(
-                child: SideMenu(),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-} */
